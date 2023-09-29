1caa67dcef8b1a7107ab23283492ecdf8eebc283
omarebid1 <omar3bid18@gmail.com>
2023-09-29 11:35:46 +0200

    file creato

---
+++ src/test/java/com/example/springblogricette/SpringBlogRicetteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springblogricette;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBlogRicetteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
